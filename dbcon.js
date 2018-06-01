cbb415dea55ff15c47cbe9adede75d31193e22eb
Tucker Dane Walker <tuckerdwalker@gmail.com>
2018-06-01 15:36:53 -0700

    Scrubbed Password

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_walkertu',
-  password        : 'group25',
-  database	  	  : 'cs361_walkertu',
+  user            : 'cs361_[YOUR USERNAME]',
+  password        : '[YOUR PASSWORD]',
+  database	  	  : '[YOUR USERNAME]',
   dateStrings     : 'date' 
 });
 
