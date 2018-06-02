0a2da992c10a224ca128ed0017e57d82060fb57d
codingmarisa <codingmarisa@users.noreply.github.com>
2018-06-02 14:42:26 -0700

    Added form tags to createAccount.js, changed confirm password id, added the function /addUser to createAccount.handlebars which allows new users to be added to Account table when the create account form is submitted.

--- dbconOriginal.js
+++ dbconOriginal.js
@@ -7,9 +7,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_ream',
-  password        : 'group25',
-  database	  	  : 'cs361_ream',
+  user            : 'cs361_[YOUR USERNAME]',
+  password        : '[YOUR PASSWORD]',
+  database	  	  : 'cs361_[YOUR USERNAME]',
   dateStrings     : 'date' 
 });
 
