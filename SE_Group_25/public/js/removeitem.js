fca7af801aaeaf64e05dc42e28a5c2221ac76f61
Aaron Didner <didnera@flip1.engr.oregonstate.edu>
2018-06-03 14:09:11 -0700

    deleted redudent accidental repository

--- SE_Group_25/public/js/removeitem.js
+++
@@ -1,9 +0,0 @@
-function removeitem(id){
-    $.ajax({
-        url: '/inventory/contents' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
