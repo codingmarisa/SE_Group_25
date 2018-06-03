f98bdcea75dbaf0b29ebca6f1a675c60ed239003
oraikus <oraikus@gmail.com>
2018-06-03 10:24:43 -0700

    WIP Navbar / Update Account / Delete Account
    Added handlebars partial for navbar and adapted create account page for
    use with update account and added a delete account button to the page as
    well.

--- dbcon.js
+++ dbcon.js
@@ -6,11 +6,25 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_[YOUR USERNAME]',
-  password        : '[YOUR PASSWORD]',
-  database	  	  : 'cs361_[YOUR USERNAME]',
+  host            : '127.0.0.1:3306',
+  user            : 'root',
+  password        : 'root',
+  database        : 'cs361_branchn',
   dateStrings     : 'date' 
 });
 
 module.exports.pool = pool;
+
+
+
+// var mysql = require('mysql');
+// var pool = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'classmysql.engr.oregonstate.edu',
+//   user            : 'cs361_[YOUR USERNAME]',
+//   password        : '[YOUR PASSWORD]',
+//   database	  	  : 'cs361_[YOUR USERNAME]',
+//   dateStrings     : 'date' 
+// });
+
+// module.exports.pool = pool;
