28c41005e94985d37020e4186d6acf719e65a555
oraikus <oraikus@gmail.com>
2018-06-03 12:16:20 -0700

    WIP Commit
    Maintenance

--- dbcon.js
+++ dbcon.js
@@ -6,25 +6,11 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : '127.0.0.1:3306',
-  user            : 'root',
-  password        : 'root',
-  database        : 'cs361_branchn',
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs361_[YOUR USERNAME]',
+  password        : '[YOUR PASSWORD]',
+  database	  	  : 'cs361_[YOUR USERNAME]',
   dateStrings     : 'date' 
 });
 
 module.exports.pool = pool;
-
-
-
-// var mysql = require('mysql');
-// var pool = mysql.createPool({
-//   connectionLimit : 10,
-//   host            : 'classmysql.engr.oregonstate.edu',
-//   user            : 'cs361_[YOUR USERNAME]',
-//   password        : '[YOUR PASSWORD]',
-//   database	  	  : 'cs361_[YOUR USERNAME]',
-//   dateStrings     : 'date' 
-// });
-
-// module.exports.pool = pool;
