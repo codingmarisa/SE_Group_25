62c3ba1c6f99e983061b98b0b66d7e4e93552f66
adidner <didnera@oregonstate.edu>
2018-06-03 13:01:47 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 
+app.use('/login', require('./login.js'), require('./inventory.js'));
 app.use('/inventory', require('./inventory.js'));
-app.use('/login', require('./login.js'));
 app.use('/createAccount', require('./createAccount.js'));
-
+app.use('/updateAccount', require('./updateAccount.js'));
 
 app.use(function(req,res){
   res.status(404);
